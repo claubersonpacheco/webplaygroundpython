d6a6955008e909dee0fc3b8ab8cad551b01ed5ea
caubinho <caubinho@gmail.com>
2025-05-24 11:35:00 +0200

    Actualization

--- registration/tests.py
+++ registration/tests.py
@@ -1,3 +1,14 @@
+from os.path import exists
+
 from django.test import TestCase
+from .models import Profile
+from django.contrib.auth.models import User
 
 # Create your tests here.
+class ProfileTestClass(TestCase):
+    def setUp(self):
+        User.objects.create(username='testuser', email='teste@test.com', password='test1234')
+
+    def test_profile_exists(self):
+        exists = Profile.objects.filter(user__username='testuser').exists()
+        self.assertEqual(exists, True)
